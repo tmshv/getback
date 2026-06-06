0614e01cf3969c2be562308a9896942bbb211179
tmshv <naked@protonmail.com>
2026-06-07 01:39:54 +0300

    Add Runner mount() bootstrapping Pixi with atlas, layers, and ticker

--- packages/game/src/index.ts
+++ packages/game/src/index.ts
@@ -1,3 +1,11 @@
 // @getback/game public surface.
-// mount() and the render exports are added in later tasks (Runner).
-export {};
+export { mount } from "./Runner.js";
+export type { MountOptions } from "./Runner.js";
+export { computeLetterbox } from "./render/letterbox.js";
+export type { LetterboxResult } from "./render/letterbox.js";
+export { selectFrame } from "./render/AnimationSystem.js";
+export type { AnimInput, AnimOutput } from "./render/AnimationSystem.js";
+export { densityToFrame } from "./render/GrassRenderer.js";
+export { frameName, frameFlipX, FRAME_GRID, FRAME_NAMES } from "./atlas/frames.js";
+export type { EntityKind, AnimState } from "./atlas/frames.js";
+export * from "./config.js";
